7203fccb5eb1aafd7ebcb02c845c4073047ebd91
Javier Cepeda <erjavierc@gmail.com>
2022-08-24 23:20:45 -0400

    proto

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-const nat = {
-    name: "nat",
-    age: 20,
-    approvedClass: [
-        "html",
-        "css"
-    ],
-    approveClass(approved){
-        
-        // make nat approve more
-        this.approvedClass.push(approved);
-    }, 
+// const nat = {
+//     name: "nat",
+//     age: 20,
+//     approvedClass: [
+//         "html",
+//         "css"
+//     ],
+//     approveClass(approved){
 
-};
+//         // make nat approve more
+//         this.approvedClass.push(approved);
+//     }, 
 
+// };
 
+const students = (name, age, approvedClass) => {
+    this.name = name;
+    this.age = age;
+    this.approvedClass = approvedClass;
+
+}
+
+students.prototype.approveClass = function(approved) {
+    this.approvedClass.push(approved)
+}
+
+const juana = new students(
+    "juana", 19,["videogame", "css"]
+)
